007595cd694ca0f9a3b3802ac18550b04ab7a4e6
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-10-09 14:53:34 -0300

    Timer do projeto

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -9,7 +9,7 @@ use Livewire\Component;
 
 class Create extends Component
 {
-    public bool $modal = true;
+    public bool $modal = false;
     public function render()
     {
         return view('livewire.proposals.create');
